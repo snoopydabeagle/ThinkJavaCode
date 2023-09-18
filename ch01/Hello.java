bf1039a48ef1e39f8d04fb0cf7222518cf7e87fe
snoopydabeagle <145195769+snoopydabeagle@users.noreply.github.com>
2023-09-18 21:56:27 +0200

    Update Hello.java

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         // generate some simple output
         System.out.println("Hello, World!");
+        // I was trying to create a witty question
         System.out.print("How are you?");
     }
 
